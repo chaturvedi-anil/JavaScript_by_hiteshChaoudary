719c8e8f00328ffa2198fb750b3d59fba55c8b89
chaturvedi-anil <chaturvedia560@gmail.com>
2023-09-15 14:29:49 +0530

    completed control flow

---
+++ javascript/controllflow/truthy.js
@@ -0,0 +1,23 @@
+// if else
+// switch
+
+// falsy values 
+// false , 0, -0, bigInt - 0n, "", null, undefined, NaN
+
+// truthy value
+// "0", " ", 'false',  [], {}, function(){}
+
+
+// nullish coalescing operator (??) : null undefined
+
+// let val1 = 5 ?? 10  // first value if present
+// console.log(val1); // 5
+
+// let val1 = null ?? 10  // first value if present
+// console.log(val1); // 10
+
+// let val1 = undefined ?? 15  // first value if present
+// console.log(val1); // 15
+
+// let val1 = null ?? 20 ?? 30 // first value if present
+// console.log(val1); // 20
